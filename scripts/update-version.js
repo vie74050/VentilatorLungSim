59d248add7fc316706de67b40042c26ac0bd0b4a
vie_bcit <vienna_ly@bcit.ca>
2026-07-16 14:27:29 -0700

    Refactor code

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,8 +1,10 @@
 (function() {
 
+    // gh workflow build piepline will update the version on push to master, but locally we don't need to do anything
     async function updateVersion() {
         try {
             // github Pages workflow build artifact will generate this file with version info on push to master
+            // locally, does not need one
             const res = await fetch("version-info.json"); 
             if (!res.ok) return;
 
